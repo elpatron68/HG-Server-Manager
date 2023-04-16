02c5157f9e4f559b02ea50b56c6f19c9e27d2656
elpatron68 <elpatron@cepheus.uberspace.de>
2023-04-16 13:08:49 +0200

    Remove unused imports

--- HG-ServerUI/MainWindow.xaml.cs
+++ HG-ServerUI/MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Navigation;
 using System.Windows.Threading;
 using Discord.WebSocket;
 using Discord;
