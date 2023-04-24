8ee4650f08b8f1a5641381b0673daefd0aef7f31
elpatron68 <elpatron@cepheus.uberspace.de>
2023-04-24 19:31:10 +0200

    Penalty delete icon

--- HG-ServerUI/Penaltyviewer.xaml.cs
+++ HG-ServerUI/Penaltyviewer.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 using SkiaSharp;
 using Svg.Skia;
 
@@ -16,35 +18,35 @@ namespace HG_ServerUI
     public partial class Penaltyviewer : MetroWindow
     {
         private List<Penalty> penaltyList = new();
+        private SettingsModel model;
         public Penaltyviewer(SettingsModel settingsModel)
         {
+            model = settingsModel;
             InitializeComponent();
-            var PenList= ConvertSvg2Png(settingsModel.Snapsdirectory);
-            DgPenalties.ItemsSource = PenList;
+            penaltyList = ConvertSvg2Png();
+            DgPenalties.ItemsSource = penaltyList;
         }
 
-        private List<Penalty> ConvertSvg2Png(string svgPath)
+        private List<Penalty> ConvertSvg2Png()
         {
-            foreach(string svgFile in Directory.GetFiles(svgPath, "*.svg"))
+            foreach(string svgFile in Directory.GetFiles(model.Snapsdirectory, "*.svg"))
             {
                 string pngfile = Path.Combine(Path.GetDirectoryName(svgFile), Path.GetFileNameWithoutExtension(svgFile) + ".png");
                 Penalty penalty = new(svgFile);
                 if (!File.Exists(pngfile))
                 {
-                    using (var svg = new SKSvg())
+                    using var svg = new SKSvg();
+                    if (svg.Load(svgFile) is { })
                     {
-                        if (svg.Load(svgFile) is { })
+                        try
                         {
-                            try
+                            using (var stream = File.OpenWrite(pngfile))
                             {
-                                using (var stream = File.OpenWrite(pngfile))
-                                {
-                                    SKColor _white = SKColor.Parse("ffffff");
-                                    _ = svg.Save(stream, _white, SKEncodedImageFormat.Png, 100);
-                                }
+                                SKColor _white = SKColor.Parse("ffffff");
+                                _ = svg.Save(stream, _white, SKEncodedImageFormat.Png, 100);
                             }
-                            catch { }
                         }
+                        catch { }
                     }
                 }
                 penaltyList.Add(penalty);
@@ -69,14 +71,40 @@ namespace HG_ServerUI
 
         private void DgPenalties_SelectedCellsChanged(object sender, System.Windows.Controls.SelectedCellsChangedEventArgs e)
         {
-            var _index = DgPenalties.SelectedIndex;
+            int _index = DgPenalties.SelectedIndex;
             ImgPenalty.Source = new BitmapImage(new Uri(penaltyList[_index].PngFullpath));
         }
 
-        private void MnDelete_Click(object sender, System.Windows.RoutedEventArgs e)
+        private async Task MnDelete_ClickAsync(object sender, System.Windows.RoutedEventArgs e)
         {
-            IList rows = DgPenalties.SelectedItems;
-
+            _ = await this.ShowMessageAsync("Not implemented",
+                    "This funtion is not yet implemented.?",
+                    MessageDialogStyle.Affirmative);
+            //ImgPenalty.Source = null;
+            //IList rows = DgPenalties.SelectedItems;
+            //foreach (object? dgrow in rows)
+            //{
+            //    try
+            //    {
+            //        string? _f1 = ((Penalty)dgrow).PngFullpath;
+            //        string? _f2 = ((Penalty)dgrow).SvgFullpath;
+            //        File.Delete(_f1);
+            //        File.Delete(_f2);
+            //        //foreach(var penrow in penaltyList)
+            //        //{
+            //        //    if(penrow.PngFullpath == _f1)
+            //        //    {
+            //        //        penaltyList.Remove(penrow);
+            //        //        return;
+            //        //    }
+            //        //}
+            //    }
+            //    catch(Exception ex)
+            //    {
+            //    }
+            //    penaltyList = new();
+            //    penaltyList = ConvertSvg2Png();
+        }
         }
     }
 }
